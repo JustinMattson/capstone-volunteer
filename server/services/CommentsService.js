08945d3cb3da9ca51576b124be0a7c28a873d4c5
Justin Mattson <justin.mattson@outlook.com>
2020-06-18 09:28:26 -0600

    commentsby job id ready to test.

--- server/services/CommentsService.js
+++ server/services/CommentsService.js
@@ -2,53 +2,53 @@ import { dbContext } from "../db/DbContext";
 import { BadRequest } from "../utils/Errors";
 
 class CommentsService {
-    async getAll(query = {}) {
-        let comments = await dbContext.Comments.find(query).populate(
-            "creator",
-            "name picture"
-        );
-        return comments;
+  async getAll(query = {}) {
+    let comments = await dbContext.Comments.find(query).populate(
+      "creator",
+      "name picture"
+    );
+    return comments;
+  }
+  async getCommentsByJobId(id) {
+    let data = await dbContext.Comments.findOne({ _id: id }).populate(
+      "creator",
+      "name picture"
+    );
+    if (!data) {
+      throw new BadRequest("Invalid Id");
     }
-    async getCommentById(id) {
-        let data = await dbContext.Comments.findOne({ _id: id }).populate(
-            "creator",
-            "name picture"
-        );
-        if (!data) {
-            throw new BadRequest("Invalid Id");
-        }
-        return data;
+    return data;
+  }
+  async create(rawData) {
+    let data = await dbContext.Comments.create(rawData);
+    return data;
+  }
+  async edit(id, userEmail, update) {
+    let data = await dbContext.Comments.findOneAndUpdate(
+      {
+        _id: id,
+        creatorEmail: userEmail,
+      },
+      update,
+      {
+        new: true,
+      }
+    );
+    if (!data) {
+      throw new BadRequest("Invalid ID");
     }
-    async create(rawData) {
-        let data = await dbContext.Comments.create(rawData);
-        return data;
+    return data;
+  }
+  async delete(id, userEmail) {
+    let data = await dbContext.Comments.findOneAndDelete({
+      _id: id,
+      creatorEmail: userEmail,
+    });
+    if (!data) {
+      throw new BadRequest("Invalid ID");
     }
-    async edit(id, userEmail, update) {
-        let data = await dbContext.Comments.findOneAndUpdate(
-            {
-                _id: id,
-                creatorEmail: userEmail,
-            },
-            update,
-            {
-                new: true,
-            }
-        );
-        if (!data) {
-            throw new BadRequest("Invalid ID");
-        }
-        return data;
-    }
-    async delete(id, userEmail) {
-        let data = await dbContext.Comments.findOneAndDelete({
-            _id: id,
-            creatorEmail: userEmail,
-        });
-        if (!data) {
-            throw new BadRequest("Invalid ID");
-        }
-        return data;
-    }
+    return data;
+  }
 }
 
 export const commentsService = new CommentsService();
