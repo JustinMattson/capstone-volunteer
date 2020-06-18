c9d31d277569201947c82807a338baaf02f5b530
Justin Mattson <justin.mattson@outlook.com>
2020-06-18 15:25:58 -0600

    added colors to profile vue

--- clientsrc/src/Pages/Profile.vue
+++ clientsrc/src/Pages/Profile.vue
@@ -1,8 +1,20 @@
 <template>
   <div class="about text-center">
     <h1>Welcome {{ profile.name }}</h1>
-    <img class="rounded" :src="profile.picture" alt="" />
+    <img class="rounded" :src="profile.picture" alt />
     <p>{{ profile.email }}</p>
+    <div style="height:100px;width:300px;background-color:#41b883">Primary</div>
+    <br />
+    <div style="height:100px;width:300px;background-color:#35495e">secondary</div>
+    <br />
+    <div style="height:100px;width:300px;background-color:#b84841">danger</div>
+    <br />
+    <div style="height:100px;width:300px;background-color:#76b841">success</div>
+    <br />
+    <div style="height:100px;width:300px;background-color:#b88341">warning</div>
+    <br />
+    <div style="height:100px;width:300px;background-color:#41b2b8">info:</div>
+    <br />
   </div>
 </template>
 
