807f81777fc55eadfc12e8e108470116945136ce
unknown <astrutledge@gmail.com>
2020-06-24 15:39:39 -0600

    home page styled

--- clientsrc/src/Pages/Home.vue
+++ clientsrc/src/Pages/Home.vue
@@ -8,29 +8,35 @@
         />
       </div>
     </div>
-    <div class="row d-flex justify-content-around my-5">
+    <div class="row text-center text-secondary py-md-5 py-1 bg-primary shadow-lg justify-content-center">
+      <div class="col-md-7">
+<h1>What is <i class="text-dark">Please Help Me!</i> ?</h1>
+<h3> We are a service commited to help those who love volunteering. Here you can find opportunities to help others with. Also, you can create opportunities for others to help you with.</h3>
+      </div>
+    </div>
+    <div class="row d-flex bg-light justify-content-around py-5 ">
       <router-link
         :to="{name: 'Jobs'}"
-        class="col-12 col-md-5 bg-primary box-height no-line d-flex align-items-center justify-content-center rounded my-1"
+        class="col-12 col-md-5 bg-primary shadow-lg border-3 box-height no-line d-flex align-items-center justify-content-center rounded my-1"
       >
-        <h1 class="text-secondary">Get help with something</h1>
+        <h1 class="text-secondary text-shadow">Get help with something</h1>
       </router-link>
       <router-link
         :to="{name: 'Jobs'}"
-        class="col-12 col-md-5 bg-secondary box-height no-line d-flex align-items-center justify-content-center rounded my-1"
+        class="col-12 col-md-5 bg-secondary shadow-lg border-4 box-height no-line d-flex align-items-center justify-content-center rounded my-1"
       >
-        <h1 class="text-primary">Find something to help with</h1>
+        <h1 class="text-primary text-shadow">Find something to help with</h1>
       </router-link>
     </div>
-    <div class="row my-5 d-flex justify-content-around">
-      <div class="card col-12 col-md-3 height-fix my-1 shadow">
+    <div class="row d-flex justify-content-around bg-secondary py-5">
+      <div class="card col-12 col-md-3 height-fix my-1 bg-primary text-secondary shadow">
         <img
           src="https://i.pinimg.com/originals/0d/c5/7b/0dc57b8d66c12736c666f157a3afa3ff.jpg"
           class="card-img-top mt-3"
           alt="error loading image"
         />
         <div class="card-body">
-          <h5 class="card-title">Sydney</h5>
+          <h4 class="card-title">Sydney</h4>
           <p class="card-text">
             <i class="fas fa-star"></i>
             <i class="fas fa-star"></i>
@@ -38,19 +44,19 @@
             <i class="fas fa-star"></i>
             <i class="fas fa-star"></i>
           </p>
-          <p
+          <h5
             class="card-text"
-          >I love this website! I run an after-school program to help keep kids busy and off the streets. We use this site almost exclusively to find projects to help out with. I reccomend this to everyone I know!</p>
+          >"I love this website! I run an after-school program to help keep kids busy and off the streets. We use this site almost exclusively to find projects to help out with. I reccomend this to everyone I know!"</h5>
         </div>
       </div>
-      <div class="card col-12 col-md-3 height-fix my-1 shadow">
+      <div class="card col-12 col-md-3 height-fix my-1 bg-primary text-secondary shadow">
         <img
           src="https://headshotphotographerasheville.com/wp-content/uploads/headshots-in-asheville-3.jpg"
-          class="card-img-top mt-3"
+          class="card-img-top mt-3 "
           alt="error loading image"
         />
         <div class="card-body">
-          <h5 class="card-title">Albert</h5>
+          <h4 class="card-title">Albert</h4>
           <p class="card-text">
             <i class="fas fa-star"></i>
             <i class="fas fa-star"></i>
@@ -58,19 +64,19 @@
             <i class="fas fa-star"></i>
             <i class="fas fa-star"></i>
           </p>
-          <p
+          <h5
             class="card-text"
-          >I really enjoy this site! I've had tons of projects I've needed help with and have found plenty of people to help me out. I've also been able to return the favor by helping others with their projects as well. This has been very useful!</p>
+          >"I really enjoy this site! I've had tons of projects I've needed help with and have found plenty of people to help me out. I've also been able to return the favor by helping others with their projects as well. This has been very useful!"</h5>
         </div>
       </div>
-      <div class="card col-12 col-md-3 height-fix my-1 shadow">
+      <div class="card col-12 col-md-3 height-fix my-1 bg-primary text-seondary shadow">
         <img
           src="https://i1.wp.com/kinserstudios.com/wp-content/uploads/2019/06/man.png?fit=384%2C295&ssl=1"
           class="card-img-top mt-3"
           alt="error loading image"
         />
         <div class="card-body">
-          <h5 class="card-title">Scott</h5>
+          <h4 class="card-title">Scott</h4>
           <p class="card-text">
             <i class="fas fa-star"></i>
             <i class="fas fa-star"></i>
@@ -78,9 +84,9 @@
             <i class="fas fa-star"></i>
             <i class="fas fa-star"></i>
           </p>
-          <p
+          <h5
             class="card-text"
-          >I've been using this site for months now and found it very helpful! I use it to help others in my community with various jobs they have going on. It gives me opportunities to learn new skills and connect with those around me.</p>
+          >"I've been using this site for months now and found it very helpful! I use it to help others in my community with various jobs they have going on. It gives me opportunities to learn new skills and connect with those around me."</h5>
         </div>
       </div>
     </div>
@@ -94,7 +100,7 @@
         </a>
       </div>
       <div class="col-4">
-        Kevin Lane
+        Kevin Lane<br/>
         <a href="https://www.linkedin.com/in/kevin-lane-2422b51b0/" target="_blank">
           <i class="fa fa-linkedin-square text-black p-1"></i>
         </a>
@@ -158,5 +164,14 @@ export default {
 }
 .text-black {
   color: black;
+}
+.border-3 {
+  border: 12px solid #35495e !important;
+}
+.border-4 {
+  border: 12px solid #41b883 !important;
+}
+.text-shadow {
+text-shadow: 1px 1px black;
 }
 </style>
