cae3fba2d149935a035460b4a2055ffc76e949d6
Justin Mattson <justin.mattson@outlook.com>
2020-06-24 13:58:06 -0600

    going to start ratings component

--- clientsrc/src/components/JobsRequestedComponent.vue
+++ clientsrc/src/components/JobsRequestedComponent.vue
@@ -1,17 +1,64 @@
 <template>
-  <div class="requesterJobs container">{{requesterJob.title}}</div>
+  <div class="requesterJobs container">
+    {{requesterJob.title}}
+    <!-- Color 1 -->
+    <div class="row bg-light border border-secondary rounded-lg mb-1 shadow" v-if="rIndex == 0">
+      <div class="col-12 col-md-6 d-flex justify-content-between">
+        <router-link
+          :to="{ name: 'job', params: { jobId: requesterJob.id } }"
+        >{{requesterJob.title}}</router-link>
+        <span class="unbold">{{when}}</span>
+      </div>
+      <div class="col-12 col-md-6 d-flex justify-content-between">
+        <span class="unbold">Status: {{requesterJob.jobStatus}}</span>
+        <span title="Rate Volunteer(s)">
+          <i class="fas fa-star-half-alt text-warning action" alt="Ratings" @click="toggleRatings"></i>
+        </span>
+      </div>
+      <!-- Ratings -->
+      <div v-show="volunteerRatings" class="border border-top">
+        <!-- FIXME Ratings Component goes here -->
+      </div>
+    </div>
+    <!-- Alt Color -->
+  </div>
 </template>
 
 
 <script>
+import moment from "moment";
 export default {
   name: "requesterJob",
   props: ["requesterJob"],
   data() {
-    return {};
+    return {
+      volunteerRatings: false
+    };
   },
-  computed: {},
-  methods: {},
+  computed: {
+    rIndex() {
+      let num = this.$store.state.postedJobs.findIndex(
+        q => q.id == this.requesterJob.id
+      );
+      return num % 2;
+    },
+    when() {
+      if (this.requesterJob.startDate == this.requesterJob.endDate) {
+        return moment(String(this.requesterJob.startDate)).format("MM/DD/YYYY");
+      } else {
+        return (
+          moment(String(this.requesterJob.startDate)).format("MM/DD/YYYY") +
+          " - " +
+          moment(String(this.requesterJob.endDate)).format("MM/DD/YYYY")
+        );
+      }
+    }
+  },
+  methods: {
+    toggleRatings() {
+      this.volunteerRatings = !this.volunteerRatings;
+    }
+  },
   components: {}
 };
 </script>
