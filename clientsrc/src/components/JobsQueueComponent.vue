1c9150e8a7f688f9f4a724b3276f76e6e3eaed48
Justin Mattson <justin.mattson@outlook.com>
2020-06-24 09:28:07 -0600

    building more components

---
+++ clientsrc/src/components/JobsQueueComponent.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="jobsInQueue container">
+    <div class="row">
+      <div class="col-12"></div>
+    </div>
+  </div>
+</template>
+
+
+<script>
+export default {
+  name: "jobsInQueue",
+  props: ["queue"],
+  data() {
+    return {};
+  },
+  computed: {},
+  methods: {},
+  components: {}
+};
+</script>
+
+
+<style scoped>
+</style>
